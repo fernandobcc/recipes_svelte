6485e86eba0b1d4ebee0f73622dd1c30d4fc96eb
fernandobcc <fernandoxico@gmail.com>
2022-12-25 14:59:42 -0300

    Updates

--- app/immutable/chunks/2-6a494080.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-9ffdf46c.js";export{t as component};
---
+++ internal/immutable/chunks/2-19115428.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fed10843.js";export{t as component};
